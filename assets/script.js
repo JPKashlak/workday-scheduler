205b170c2ba2156df9ff39c63bffa6686d7493b1
jpkashlak <jpkashlak@gmail.com>
2021-01-09 16:32:58 -0600

    color-coding time slots via if/else

--- assets/script.js
+++ assets/script.js
@@ -7,45 +7,25 @@ var centralClock = function () {
     console.log(centralClock);
 };
 
+var colorCheck = function() {
+   var currentHour = moment().hours(); 
 
-
-
-
-
-$(".taskCell").on("click", function() {
-    
-    $(".taskCell p").html("<textarea>");
-
-     // var text = $(this).text().trim();
-
-    // var textInput = ("<textarea>").val(text);
-    //     $(this).replaceWith(textInput);  
-});
+    $(".row").each( function() {
+        var taskHour = this.id;
+            if (taskHour == currentHour) {
+                $(`#${taskHour} > textarea`).addClass("bg-danger");
+            }
+            else if (taskHour < currentHour) {
+                $(`#${taskHour} > textarea`).addClass("bg-secondary")
+            }
+            else {
+                $(`#${taskHour} > textarea`).addClass("bg-success")
+            }
+    });
+};
    
 
-
-
-
-
-
-
-
-var timeCheck = function() {
-
-        if (moment().diff())
-        $(".taskCell").addClass("bg-danger");
-};
- 
-
-
-
-
-
-
-
-
-
-
+colorCheck();
 centralClock();
 // document.ready end
 });
