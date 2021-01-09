89040c56fd9d1b48f44feceacb4c8b4c682295f2
jpkashlak <jpkashlak@gmail.com>
2021-01-09 10:48:41 -0600

    header clock updates every second

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,9 @@
-$("#currentDay").append(moment().format('MMMM Do YYYY, h:mm:ss a'));
-   
+// Header Clock
+var centralClock = function () {
+    var currentTime = (moment().format('MMMM Do YYYY, h:mm:ss a'));
+    $("#currentDay").html(currentTime);
+    setInterval(centralClock, 1000);
+}
+    $(document).ready(function() {
+        centralClock();
+});
